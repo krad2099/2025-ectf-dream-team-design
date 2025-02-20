60ef602861e469b1d5647b5f9f22a46895efe391
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-02-19 19:02:10 -0600

    Update decoder.c

--- decoder/src/decoder.c
+++ decoder/src/decoder.c
@@ -24,8 +24,7 @@
 #include "host_messaging.h"
 #include "simple_uart.h"
 #include "simple_crypto.h"
-#include <wolfssl/ssl.h>
-#include <wolfssl/wolfcrypt/aes.h>
+
 
 #define timestamp_t uint64_t
 #define channel_id_t uint32_t
