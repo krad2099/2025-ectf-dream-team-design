655a0714b51f53fe5289c23017cb104908a727fc
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-02-22 19:28:41 -0600

    Update gen_secrets.py

--- design/ectf25_design/gen_secrets.py
+++ design/ectf25_design/gen_secrets.py
@@ -1,14 +1,3 @@
-"""
-Author: Ben Janis
-Date: 2025
-
-This source file is part of an example system for MITRE's 2025 Embedded System CTF
-(eCTF). This code is being provided only for educational purposes for the 2025 MITRE
-eCTF competition, and may not meet MITRE standards for quality. Use this code at your
-own risk!
-
-Copyright: Copyright (c) 2025 The MITRE Corporation
-"""
 import json
 import base64
 import os
@@ -19,23 +8,20 @@ def generate_secret_key():
 
 def encrypt_data(data, encryption_key):
     fernet = Fernet(encryption_key)
-    encrypted_data = fernet.encrypt(data.encode())
-    return encrypted_data
+    return fernet.encrypt(data.encode())
 
 def main():
     encryption_key = Fernet.generate_key()
     secrets = {
-        "secret_key": generate_secret_key(),
-        "encryption_key": base64.urlsafe_b64encode(Fernet.generate_key()).decode('utf-8')
+        "some_secrets": generate_secret_key(),
+        "encryption_key": encryption_key.decode('utf-8')
     }
-    
     secrets_json = json.dumps(secrets, indent=4)
     encrypted_secrets = encrypt_data(secrets_json, encryption_key)
-    
+
     with open("secrets.json", "wb") as f:
         f.write(encrypted_secrets)
     
-    # Store the encryption key securely, not in the same location as the secrets file
     with open("encryption_key.key", "wb") as f:
         f.write(encryption_key)
 
